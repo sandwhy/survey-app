80f774515c2f7aab69dd6fcad31139835e18cdcb
sandman <sandwhen@gmail.com>
2022-06-15 14:30:01 +0700

    added frontend routes and things

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+      <div className='nav-wrapper'>
+        <a className='left brand-logo'>
+          Emaily
+        </a>
+        <ul className='right'>
+          <li>
+            <a>Login With Google</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
