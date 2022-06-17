c44ed3428b8bdb8138e87a17df96e00bbb8baa63
sandman <sandwhen@gmail.com>
2022-06-17 18:18:47 +0700

    ok

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import whiteflag from '../white_flag/whiteflag'
+import whiteflag from '../white_flag/WhiteFlg'
 
 export const fetchCurrentUser = () => axios.get('/api/current_user')
 export const apiStripe = (token) => axios.post('/api/stripe', token, { Authorization: "Bearer " + whiteflag.stripesecretKey })
