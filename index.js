481840c890c054919d61bcf65e650cb7e0b6f211
sandman <sandwhen@gmail.com>
2022-07-07 21:22:56 +0700

    editing vars

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 
     const path = require('path')
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
     })
 }
 
