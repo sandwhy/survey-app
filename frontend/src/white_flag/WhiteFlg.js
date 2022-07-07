80c700f72dbe96b023e65935aa44b9ed352ffe60
sandman <sandwhen@gmail.com>
2022-07-07 17:39:24 +0700

    ok

--- frontend/src/white_flag/WhiteFlg.js
+++ frontend/src/white_flag/WhiteFlg.js
@@ -1,4 +1,6 @@
 module.exports = {
     stripepublicKey:process.env.stippubskey,
-    stripesecretKey:process.env.stripseckey
+    stripesecretKey:process.env.stripseckey,
+    domain:process.env.mailgundomain,
+    mgkey:process.env.mailgunkey
 }
